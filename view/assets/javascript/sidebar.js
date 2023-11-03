d0322b8c461d01835b1e34c70354aecc62dbebce
Tran Quoc Thang <quocthangtran0@gmail.com>
2023-11-03 07:04:07 +0700

    Them slider

--- view/assets/javascript/sidebar.js
+++ view/assets/javascript/sidebar.js
@@ -1,5 +1,14 @@
 newsPost = () => {
   const rotate = document.getElementById("rotate");
+  const model = document.getElementById("model-news");
+
+  if (model.style.display === "none" || model.style.display === "") {
+    model.style.display = "block";
+    model.style.transform = "translate3d(26.4px, 62.4px, 0px)";
+  } else {
+    // model.style.transform = "translate3d(26.4px, 0, 0)";
+    model.style.display = "none";
+  }
 
   rotate.classList.toggle("diagonal");
 };
---
+++ view/assets/javascript/slider.js
@@ -0,0 +1,46 @@
+const slider = document.querySelector(".slider");
+const prevButton = document.querySelector(".prev");
+const nextButton = document.querySelector(".next");
+const slides = document.querySelectorAll(".slide");
+const pageButtons = document.querySelectorAll(".page-button");
+
+let slideIndex = 0;
+
+nextButton.addEventListener("click", () => {
+  slideIndex++;
+  if (slideIndex >= slides.length) {
+    slideIndex = 0;
+  }
+  updateSlider();
+});
+
+prevButton.addEventListener("click", () => {
+  slideIndex--;
+  if (slideIndex < 0) {
+    slideIndex = slides.length - 1;
+  }
+  updateSlider();
+});
+
+pageButtons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    slideIndex = index;
+    updateSlider();
+  });
+});
+
+function updateSlider() {
+  slider.style.transform = `translateX(-${slideIndex * 100}%)`;
+
+  // Update page buttons
+  pageButtons.forEach((button, index) => {
+    if (index === slideIndex) {
+      button.classList.add("active");
+    } else {
+      button.classList.remove("active");
+    }
+  });
+}
+
+// Initial update
+updateSlider();
